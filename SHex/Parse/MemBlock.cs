63715c70e738399ca37a862589afc168d20600c2
caesarhao <caesarhao@gmail.com>
2019-04-01 22:46:57 +0200

    continue

---
+++ SHex/Parse/MemBlock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SHex
+{
+	public class MemBlock
+	{
+		private int AddrSize;
+		private ulong StartAddr;
+		private byte[]	data;
+		public MemBlock ()
+		{
+			AddrSize = 4;
+		}
+	}
+}
+
