91b085bba4a6b70d0fd86104b5c710143f4839ab
caesarhao <caesarhao@gmail.com>
2019-04-03 23:42:40 +0200

    haha

---
+++ SHex/FileAccess/Hex/HexAccess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SHex
+{
+	
+	public class HexAccess:IFileAccess
+	{
+		 
+		public HexAccess ()
+		{
+			
+		}
+		public HexAccess(string fileName)
+		{
+			
+		}
+		public HexAccess(string[] fileLines)
+		{
+			
+		}
+
+	}
+}
+
--- SHex/FileAccess/Hex/HexRecord.cs
+++ SHex/FileAccess/Hex/HexRecord.cs
@@ -1,10 +1,10 @@
 ﻿using System;
-
+using System.Text.RegularExpressions;
 namespace SHex
 {
-	public class Record
+	public class HexRecord
 	{
-		public enum RecordType
+		public enum RecordType : byte
 		{
 			Data = 0x00,
 			EoF = 0x01,
@@ -12,16 +12,22 @@ namespace SHex
 			StrtSegAddr = 0x03,
 			ExtLineAddr = 0x04,
 			StrtLineAddr = 0x05
-		}
+		};
+		public enum ErrorNum : byte
+		{
+			NoErr = 0x00,
+
+		};
+		private ErrorNum errNo;
 		private byte startCode; // : 1 byte
 		private byte byteCount; // 1 byte
 		private ushort address; // 2 bytes
-		private byte recordType; // 1 byte
+		private RecordType recordType; // 1 byte
 		private byte[] data; //
 		private byte crc; // 1 byte
-		public Record()
+		public HexRecord()
 		{
-			
+			this.errNo = ErrorNum.NoErr;
 		}
 		public static int hex2dec(byte c)
 		{
@@ -35,6 +41,11 @@ namespace SHex
 				return -1;
 			}
 		}
+		public static int hex2dec(char c)
+		{
+			return(hex2dec(Convert.ToByte(c)));
+		}
+
 		public static byte getFrstByte(string line){
 			byte retu = 0;
 			if (line.Length > 1) 
@@ -44,6 +55,10 @@ namespace SHex
 			}
 			return retu;
 		}
+		public static string dec2hex(byte h)
+		{
+			return String.Format ("{0,2:X2}", h);
+		}
 		public static byte getByte(char c1, char c2){
 			byte retu = 0;
 			retu = hex2dec (c1) << 4;
@@ -52,32 +67,14 @@ namespace SHex
 		}
 		public bool parse(string line)
 		{
-			if (':' != line [0]) {
-			} else {
-				line = line.Substring (1);
-				if ((line.Length) % 2 != 0) {
-				} else {
-					
-				}
-			}
+			Regex rgx = new Regex (@"^:(?<byteCount>[A-Fa-f0-9]{2})(?<address>[A-Fa-f0-9]{4})(?<type>[A-Fa-f0-9]{2})(?<data>[A-Fa-f0-9]*)(?<crc>[A-Fa-f0-9]{2})$");
+			Match match = rgx.Match(line);
+			byte byteCount = byte.Parse(match.Groups["byteCount"].Value);
+			ushort address = ushort.Parse(match.Groups["address"].Value);
+			byte type = byte.Parse(match.Groups["type"].Value);
+			byte crc = byte.Parse(match.Groups["crc"].Value);
+			return (this.errNo == ErrorNum.NoErr);
 		}
-	}
-	public class HexAccess:IFileAccess
-	{
-		 
-		public HexAccess ()
-		{
-			
-		}
-		public HexAccess(string fileName)
-		{
-			
-		}
-		public HexAccess(string[] fileLines)
-		{
-			
-		}
-
 	}
 }
 
