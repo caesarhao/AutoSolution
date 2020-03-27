4312ecc39bac2e634de70ff0841f7c8fe46b358d
Hao ZHANG <caesarhao@gmail.com>
2020-03-27 18:04:20 +0100

    Update BaseType.cs

--- EasyOS/Data/BaseType.cs
+++ EasyOS/Data/BaseType.cs
@@ -4,17 +4,17 @@ namespace EasyOS
 {
 	public enum BaseType
 	{
-		BOOL,
-		UINT8,
-		SINT8,
-		UINT16,
-		SINT16,
-		UINT32,
-		SINT32,
-		UINT64,
-		SINT64,
-		FLOAT32,
-		DOUBLE64
+		BOOL = 0, // bool in C#
+		UINT8 = 1, // byte in C#
+		SINT8 = 2, // sbyte in C#
+		UINT16 = 3, // ushort in C#
+		SINT16 = 4, // short in C#
+		UINT32 = 5, // uint in C#
+		SINT32 = 6, // int in C#
+		UINT64 = 7, // ulong in C#
+		SINT64 = 8, // long in C#
+		FLOAT32 = 9, // float in C#
+		DOUBLE64 = 10 // double in C#
 	}
 }
 
