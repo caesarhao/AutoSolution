865014e28659c93f0422921b47ccdd2585f3b2fb
Hao ZHANG <caesarhao@gmail.com>
2020-03-27 16:11:39 +0100

    Update BaseData.cs

--- EasyOS/Data/BaseData.cs
+++ EasyOS/Data/BaseData.cs
@@ -4,12 +4,13 @@ namespace EasyOS
 {
 	public abstract class BaseData
 	{
-		public string name{ get; set;}
+		public string shortName{ get; set;}
+		public string description{ get; set;}
 		public BaseData ()
 		{
 		}
 		override public string ToString(){
-			return name;
+			return shortName;
 		}
 	}
 }
