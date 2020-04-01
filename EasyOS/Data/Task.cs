6cc2f60cd23f52855db8d83d660dc41c10dbc09e
Hao ZHANG <caesarhao@gmail.com>
2020-04-01 17:27:10 +0200

    Update Task.cs

--- EasyOS/Data/Task.cs
+++ EasyOS/Data/Task.cs
@@ -5,7 +5,24 @@ namespace EasyOS
 {
 	public class Task:BaseData
 	{
-		List<Process> processes;
+		public enum ERaster{
+			[Description("ONCE")]
+			E_ONCE,
+			[Description("1 ms")]
+			E_1ms,
+			[Description("10 ms")]
+			E_10ms,
+			[Description("100 ms")]
+			E_100ms,
+			[Description("1 s")]
+			E_1s,
+			[Description("10 s")]
+			E_10s,
+			[Description("100 s")]
+			E_100s
+		};
+		public ERaster raster;
+		public List<Process> processes;
 		public Task ()
 		{
 			processes = new List<Process> ();
