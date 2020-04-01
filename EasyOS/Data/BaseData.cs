c037f1c256352ffd221b4b05dd0eb05ca8bd8ecb
Hao ZHANG <caesarhao@gmail.com>
2020-04-01 17:19:49 +0200

    Update BaseData.cs

--- EasyOS/Data/BaseData.cs
+++ EasyOS/Data/BaseData.cs
@@ -4,18 +4,30 @@ namespace EasyOS
 {
 	public abstract class BaseData
 	{
+		private static List<this.GetType()> allItems = new List<this.GetType()>();
 		public string name{ get; set;}
 		public string description{ get; set;}
 		public BaseData ()
 		{
+			allItems.Add(this);
+		}
+		public static BaseData[] GetAll(){
+			return allItems.ToArray();
+		}
+		public static BaseData GetWithName(string name){
+			foreach(var item in allItems){
+				if (name.CompareTo(item.name) == 0){
+					return item;
+				}
+			}
+			return null;
 		}
 		override public string ToString(){
 			return name;
 		}
-//		public abstract string SaveAsXml();
-//		public abstract string GenCodeC();
-//		public abstract string GenCodePython();
-//		public abstract string GenCodeLua();
+//		public abstract string[] SaveToXml();
+//		public abstract BaseData ParseFromXml(string[] lines);
+//		public abstract string[] GenCode();
 	}
 }
 
